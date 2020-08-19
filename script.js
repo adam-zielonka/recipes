a666843e7b57119f4fc5c51887a7bf728a81dc11
Adam Zielonka <root@adamzielonka.pro>
2020-08-19 23:18:29 +0200

    languagetool host

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const { exec } = require("child_process")
 exec("ls *.md -1a", (error, out, err) => {
   for (const i of out.split('\n').filter(a => a)) {
     const file = fs.readFileSync(i, "utf8").replace('  \r\n', '\r\n')
-    if (i !== 'README.md' && i !== 'SUMMARY.md') exec(`curl -s --data "language=pl-PL&text=${file}" http://localhost:8010/v2/check`, (error, out, err) => {
+    if (i !== 'README.md' && i !== 'SUMMARY.md') exec(`curl -s --data "language=pl-PL&text=${file}" http://languagetool:8010/v2/check`, (error, out, err) => {
       console.log(i)
       console.log(JSON.stringify(JSON.parse(out).matches, null, 4))
     })
