bb49a1e57560ac61498dc1271e91c7f03f81f096
Adam Zielonka <root@adamzielonka.pro>
2020-08-19 23:35:47 +0200

    File name in log

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ const fs = require("fs")
 const { exec } = require("child_process")
 
 exec("ls *.md -1a", (error, out, err) => {
-  for (const i of out.split('\n').filter(a => a)) {
-    const file = fs.readFileSync(i, "utf8").replace('  \r\n', '\r\n')
-    if (i !== 'README.md' && i !== 'SUMMARY.md') exec(`curl -s --data "language=pl-PL&text=${encodeURI(file)}" http://languagetool:8010/v2/check`, (error, out, err) => {
-      console.log(file)
+  for (const fileName of out.split('\n').filter(a => a)) {
+    const file = fs.readFileSync(fileName, "utf8").replace('  \r\n', '\r\n')
+    if (fileName !== 'README.md' && fileName !== 'SUMMARY.md') exec(`curl -s --data "language=pl-PL&text=${encodeURI(file)}" http://languagetool:8010/v2/check`, (error, out, err) => {
+      console.log(fileName)
       console.log(JSON.stringify(JSON.parse(out).matches, null, 4))
     })
   }
