a0e96a8271def56e4d8ed9dd8502a638df2a37c5
Adam Zielonka <root@adamzielonka.pro>
2020-08-20 09:25:12 +0000

    Only lines with errors

--- script.js
+++ script.js
@@ -3,12 +3,13 @@ const { exec } = require("child_process")
 
 exec("ls *.md -1a", (error, out, err) => {
   for (const fileName of out.split('\n').filter(a => a)) {
-    const file = fs.readFileSync(fileName, "utf8").replace('  \r\n', '\r\n').replace('\r','')
+    const file = fs.readFileSync(fileName, "utf8").replace('  \r\n', '\r\n').replace('\r','').replace('\n  ','\n').replace('\n ','\n')
     if (fileName !== 'README.md' && fileName !== 'SUMMARY.md') {
       file.split('\n').forEach((line, i) => {
         exec(`curl -s --data "language=pl-PL&text=${encodeURI(line)}" http://languagetool:8010/v2/check`, (error, out, err) => {
-          console.log(`${fileName}:${i}`)
-          console.log(JSON.stringify(JSON.parse(out).matches, null, 4))
+          if (JSON.parse(out).matches.length) {
+            console.log(`${fileName}:${i}\n` + JSON.stringify(JSON.parse(out).matches, null, 4))
+          }
         })
       })
     }
