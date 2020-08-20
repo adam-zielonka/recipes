e5c3048fae6d8e6b035af7f7dbc1e8ae90622ef5
Adam Zielonka <root@adamzielonka.pro>
2020-08-20 09:31:27 +0000

    Delete spaces

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const { exec } = require("child_process")
 
 exec("ls *.md -1a", (error, out, err) => {
   for (const fileName of out.split('\n').filter(a => a)) {
-    const file = fs.readFileSync(fileName, "utf8").replace('  \r\n', '\r\n').replace('\r','').replace('\n  ','\n').replace('\n ','\n')
+    const file = fs.readFileSync(fileName, "utf8").replace('  \r\n', '\r\n').replace('\r','').replace(/\n\s*/,'\n')
     if (fileName !== 'README.md' && fileName !== 'SUMMARY.md') {
       file.split('\n').forEach((line, i) => {
         exec(`curl -s --data "language=pl-PL&text=${encodeURI(line)}" http://languagetool:8010/v2/check`, (error, out, err) => {
