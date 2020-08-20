0eb7cdcca437505a7d64022fee7fb49d661e92d6
Adam Zielonka <root@adamzielonka.pro>
2020-08-20 09:17:43 +0000

    Line by Line

--- script.js
+++ script.js
@@ -3,10 +3,14 @@ const { exec } = require("child_process")
 
 exec("ls *.md -1a", (error, out, err) => {
   for (const fileName of out.split('\n').filter(a => a)) {
-    const file = fs.readFileSync(fileName, "utf8").replace('  \r\n', '\r\n')
-    if (fileName !== 'README.md' && fileName !== 'SUMMARY.md') exec(`curl -s --data "language=pl-PL&text=${encodeURI(file)}" http://languagetool:8010/v2/check`, (error, out, err) => {
-      console.log(fileName)
-      console.log(JSON.stringify(JSON.parse(out).matches, null, 4))
-    })
+    const file = fs.readFileSync(fileName, "utf8").replace('  \r\n', '\r\n').replace('\r','')
+    if (fileName !== 'README.md' && fileName !== 'SUMMARY.md') {
+      file.split('\n').forEach((line, i) => {
+        exec(`curl -s --data "language=pl-PL&text=${encodeURI(line)}" http://languagetool:8010/v2/check`, (error, out, err) => {
+          console.log(`${fileName}:${i}`)
+          console.log(JSON.stringify(JSON.parse(out).matches, null, 4))
+        })
+      })
+    }
   }
 })
