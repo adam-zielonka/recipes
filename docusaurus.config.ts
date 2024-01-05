0f0564b3434aa79fe2db047f5b8ab8712536dd71
Adam Zielonka <root@adamzielonka.pro>
2024-01-05 13:52:54 +0100

    Migrate to docusaurus

---
+++ docusaurus.config.ts
@@ -0,0 +1,61 @@
+import {themes as prismThemes} from 'prism-react-renderer';
+import type {Config} from '@docusaurus/types';
+import type * as Preset from '@docusaurus/preset-classic';
+
+const config: Config = {
+  title: 'Przepisy',
+  tagline: 'Głodny człowiek jeść',
+  url: 'https://przepisy.adamzielonka.pro/',
+  baseUrl: '/',
+  organizationName: 'adam-zielonka',
+  projectName: 'recipes',
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  i18n: {
+    defaultLocale: 'pl',
+    locales: ['pl'],
+  },
+
+  presets: [
+    [
+      'classic',
+      {
+        docs: {
+          routeBasePath: "/",
+        },
+        blog: false,
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  themes: [
+    ["@easyops-cn/docusaurus-search-local", {
+      indexBlog: false,
+      docsRouteBasePath: "/",
+    }]
+  ],
+
+  themeConfig: {
+    navbar: {
+      title: 'Przepisy',
+      items: [
+        {
+          href: "https://github.com/adam-zielonka/recipes",
+          label: "GitHub",
+          position: "right",
+        },
+      ],
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  } satisfies Preset.ThemeConfig,
+};
+
+export default config;
